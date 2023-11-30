04c0ff06487552a46b1c06259cd30ef335ef4a80
xjd <xjd@rainbowbr.cn>
2023-11-30 16:50:21 +0800

    fix: 时区指定

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "tgbot/internal/bot"
+import (
+	"fmt"
+	"tgbot/internal/bot"
+	"time"
+)
 
 func main() {
+	// 设置全局时区
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return
+	}
+	time.Local = loc
 	bot.StartBot()
 }
