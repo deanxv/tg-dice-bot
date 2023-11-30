77e1223f9a285b582635c56b70ba33448bf8a2fc
xjd <xjd@rainbowbr.cn>
2023-11-30 16:51:48 +0800

    fix:

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"tgbot/internal/bot"
-	"time"
 )
 
 func main() {
-	// 设置全局时区
-	loc, err := time.LoadLocation("Asia/Shanghai")
-	if err != nil {
-		fmt.Println("Error loading location:", err)
-		return
-	}
-	time.Local = loc
 	bot.StartBot()
 }
